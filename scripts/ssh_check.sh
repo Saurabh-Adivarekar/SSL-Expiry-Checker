4987062087e7db60e84b3b3950a75925a537a4e9
Saurabh-Adivarekar <87898893+Saurabh-Adivarekar@users.noreply.github.com>
2023-08-22 07:03:38 +0530

    Update ssh_check.sh

--- scripts/ssh_check.sh
+++ scripts/ssh_check.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "SLACK_WEBHOOK_URL: $SLACK_WEBHOOK_URL"
-echo "DOMAINS_SECRET: $DOMAINS_SECRET"
+WEBHOOK_URL="${SLACK_WEBHOOK_URL}"
+IFS=$'\n' read -rd '' -a DOMAINS <<< "${DOMAINS_SECRET}"
 
-WEBHOOK_URL="$SLACK_WEBHOOK_URL"
-
-while IFS= read -r DOMAIN; do
-    EXPIRY_DATE=$(echo | openssl s_client -servername "$DOMAIN" -connect "$DOMAIN":443 2>/dev/null | openssl x509 -noout -enddate | cut -d "=" -f 2)
+for DOMAIN in "${DOMAINS[@]}"; do
+    EXPIRY_DATE=$(openssl s_client -servername "$DOMAIN" -connect "$DOMAIN":443 2>/dev/null | openssl x509 -noout -enddate | awk -F= '{print $2}')
     EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
     CURRENT_TIMESTAMP=$(date +%s)
     DAYS_LEFT=$(( ($EXPIRY_TIMESTAMP - $CURRENT_TIMESTAMP) / 86400 ))
@@ -14,4 +12,5 @@ while IFS= read -r DOMAIN; do
     ALERT_MESSAGE="SSL Expiry Alert\n   * Domain : $DOMAIN\n   * Warning : The SSL certificate for $DOMAIN will expire in $DAYS_LEFT days."
 
     curl -X POST -H "Content-type: application/json" --data "{\"text\":\"$ALERT_MESSAGE\"}" "$WEBHOOK_URL"
-done <<< "$DOMAINS"
+done
+
